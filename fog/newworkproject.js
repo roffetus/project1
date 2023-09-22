3f0d6a2c307a8b8f98c06695a3d1806b0214b890
roman <example@gmail.com>
2023-09-22 21:37:38 +0300

    no

--- fog/newworkproject.js
+++ fog/newworkproject.js
@@ -10,6 +10,21 @@ const personalMovieDB = {
       for(let i =0;i<2;i++){
 const a = prompt('one of the last movies i watched?',''),
       b = prompt('how much would you rate it?','');
+      if(a!=null && b!=null && a!='' && b!='' && a.length<50){
+            console.log('done')//!='' not equal to the empty string for answer
+      } else{
+            console.log('error')
+            i--;
+      }
+      if(personalMovieDB.count<10){
+            console.log('few films watched')
+      }else if(personalMovieDB.count>=10 && personalMovieDB.count<10){
+      console.log ('you are classic watcher')
+      }else if(personalMovieDB.count>=30){
+      console.log('you are a film buff')
+      }else{
+      console.log('an error occurred')
+      }
 personalMovieDB.movies[a]= b;
 
       }
